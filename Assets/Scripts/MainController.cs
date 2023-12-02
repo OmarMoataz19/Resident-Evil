145a9bd6dc0cbe0644b9f8df75770361d0f4211f
OmarMoataz19 <omarmoataz1942@gmail.com>
2023-12-02 23:23:24 +0200

    added empty scripts

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    private int hp;
+    private int gold;
+    //knife durability..
+    // inventory
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
