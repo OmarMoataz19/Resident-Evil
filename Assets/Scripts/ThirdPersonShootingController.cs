547b99bdbae736905dd38b1caf5d36944a086724
OmarMoataz19 <omarmoataz1942@gmail.com>
2023-12-03 02:41:40 +0200

    fixed camera and added the aim

--- Assets/Scripts/ThirdPersonShootingController.cs
+++ Assets/Scripts/ThirdPersonShootingController.cs
@@ -9,30 +9,31 @@ public class ThirdPersonShootingController : MonoBehaviour
     public LayerMask aimColliderLayerMask = new LayerMask();
     private StarterAssetsInputs starterAssetsInputs;
     private ThirdPersonController thirdPersonController;
+    private Animator animator;
   
     //variables for controlling ads
     public float normalSensitivity = 1f;
     public float aimSensitivity = 0.5f;
     // Start is called before the first frame update
 
-    public Transform debugTransform;
     void Start()
     {
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
         thirdPersonController = GetComponent<ThirdPersonController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 mouseWorldPosition = Vector3.zero;
+
         //calculate where leon is pointing 
         Vector2 screenCenterPostition = new Vector2(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPostition);
 
         Transform targetHit = null; //will be used with hitscan..
         if(Physics.Raycast(ray, out RaycastHit hit, 999f, aimColliderLayerMask)){
-            debugTransform.position = hit.point;
             mouseWorldPosition = hit.point;
             targetHit = hit.transform; //can check to decrease hp of the zombie etc.
         }
@@ -50,7 +51,10 @@ public class ThirdPersonShootingController : MonoBehaviour
             worldAimTarget.y = transform.position.y;
             Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
 
-            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 15f ); //experimental value 15..
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f ); //experimental value 20..
+
+            //change the layers weight taking the index..
+            animator.SetLayerWeight(1, Mathf.Lerp(animator.GetLayerWeight(1), 1f, Time.deltaTime * 10f)); //lerp to make it smooth..
         }
         else{
             aimVirtualCamera.gameObject.SetActive(false);
@@ -58,8 +62,31 @@ public class ThirdPersonShootingController : MonoBehaviour
 
             //enable the rotation from the camera..
             thirdPersonController.SetRotateOnMove(true);
+
+            //change the layers weight taking the index..
+            animator.SetLayerWeight(1, Mathf.Lerp(animator.GetLayerWeight(1), 0f, Time.deltaTime * 10f)); //lerp to make it smooth..
         }
 
+        if(starterAssetsInputs.shoot)
+        {
+            //todo: add the logic to check if holding gun etc && sufficient ammo..
+            // check for the logic of automatic and semi automatic here as well..
 
+            //shoot the gun
+            Debug.Log("Shoot");
+            //check if we hit something
+            if(targetHit != null)
+            {
+                //check if we hit a zombie
+                //ZombieController zombieController = targetHit.GetComponent<ZombieController>();
+                // if(zombieController != null)
+                // {
+                //     //we hit a zombie
+                //     //todo: add the logic to check which weapon is being used and decrease the hp accordingly..
+                //     zombieController.TakeDamage(10f);
+                // }
+            }
+            starterAssetsInputs.shoot = false;
+        }
     }
 }
--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -13,6 +13,7 @@ namespace StarterAssets
 		public bool jump;
 		public bool sprint;
 		public bool aim;
+		public bool shoot;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -49,6 +50,10 @@ namespace StarterAssets
 		{
 			AimInput(value.isPressed);
 		}
+		public void OnShoot(InputValue value)
+		{
+			ShootInput(value.isPressed);
+		}
 #endif
 
 
@@ -74,6 +79,10 @@ namespace StarterAssets
 		public void AimInput(bool newAimState)
 		{
 			aim = newAimState;
+		}
+		public void ShootInput(bool newShootState)
+		{
+			shoot = newShootState;
 		}
 
 		private void OnApplicationFocus(bool hasFocus)
