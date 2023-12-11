3621db324c460b5be50c32c6c20875a8e475882a
OmarMoataz19 <omarmoataz1942@gmail.com>
2023-12-12 01:50:58 +0200

    finished HUD Pickup w/hookah

--- Assets/Inventory/ItemPickup.cs
+++ Assets/Inventory/ItemPickup.cs
@@ -1,8 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class ItemPickup : MonoBehaviour
 {
     public InventoryItem Item;
+    public TextMeshProUGUI UI;
 
     void Pickup()
     {
@@ -16,13 +19,23 @@ public class ItemPickup : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
+        //var hanafy = UI.GetComponent<TMPro.TextMeshPro>();
         if (other.CompareTag("Player"))
         {
+            UI.text = "Press E to Pickup " + Item.itemName;
             if (Input.GetKeyDown(KeyCode.E))
             {
                 Pickup();
+                UI.text="";
             }
         } 
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UI.text="";
+        }
     }
 
 
