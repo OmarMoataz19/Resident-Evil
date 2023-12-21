1ba3cd463850b47cd4d90c030b1560cda99fd758
OmarMoataz19 <omarmoataz1942@gmail.com>
2023-12-21 07:00:30 +0200

    finished

--- Assets/Scripts/ZombieMain.cs
+++ Assets/Scripts/ZombieMain.cs
@@ -139,7 +139,7 @@ public class ZombieMain : MonoBehaviour
                     }
                 }
             }
-            else if (!isStunned && !LeonAnimatorScript.isGrappled && !closeFromLeon(5f,true) && hasWeaopn 
+            else if (!isStunned && !LeonAnimatorScript.isGrappled && !closeFromLeon(2f,true) && hasWeaopn 
             && (Time.frameCount % 120 == 0) && (Random.Range(1,5) ==1)
             && ZombieAnimator.GetCurrentAnimatorStateInfo(0).IsName("Zombie Walk"))
             {
